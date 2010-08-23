b577d2a05a1ef1da180751b99c4eb965c4b0ec7c
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-23 12:30:31 +0000

    Improve script

--- createtarball.sh
+++ createtarball.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-tar czvf ../baselayout.tar.gz ../baselayout --exclude .svn --exclude createtarball.sh
+
+NAME=baselayout
+VERSION=3.3
+
+DIRNAME=$NAME-$VERSION
+
+if [ -d ../$DIRNAME ]; then
+    echo ../$DIRNAME already exists. Remove it first.
+    exit 1
+fi
+
+cp -r ../$NAME ../$DIRNAME
+tar czvf $DIRNAME.tar.gz ../$DIRNAME --exclude .git --exclude .svn --exclude `basename $0`
+rm -rf ../$DIRNAME
